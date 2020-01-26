97efa3f697c07ddc3aeacf896d41cf6bfe0573dd
123AlexDL <a1k7dchallenge@gmail.com>
2020-01-26 19:16:50 +0000

    New button, extra js.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let b=document.querySelector(`body`)
+let b=document.querySelector(`body`);
 
-b.classList.add(`container`)
+b.classList.add(`container`);
+
+function addButton(daddy){
+    let newElem=document.createElement(`button`);
+    newElem.textContent=`newButton`;
+    daddy.appendChild(newElem);
+    return newElem;
+}
+
+newB=addButton(b);
+
+newB.classList.toggle(`testB`);
+
